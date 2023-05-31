9308b0bac858a63c6047ec87458c6d9589b4f018
Ruan Gustavo <ruan.gustavo@escolar.ifrn.edu.br>
2023-05-31 14:08:44 -0300

    feat: add env variables parsing and validation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,6 @@
-import fastify from "fastify";
+import { app } from "server";
+import { env } from "env";
 
-const app = fastify();
-
-app.get("/", () => {
-  return { message: "Hello, world!" };
-});
-
-const port = 3333 || process.env.PORT;
 app
-  .listen({ port })
-  .then(() => console.log(`🚀 Server running at port ${port}`));
+  .listen({ port: env.PORT })
+  .then(() => console.log(`🚀 Server running at port ${env.PORT}`));
